909213d15417a04025ade75f95618ecc3154952e
Roblin Aurelien <roblinaurelien@gmail.com>
2019-02-02 20:29:01 +0100

    update merge

--- test/redir.c
+++ test/redir.c
@@ -6,7 +6,7 @@
 /*   By: aroblin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/02 05:52:22 by aroblin           #+#    #+#             */
-/*   Updated: 2019/02/02 06:02:25 by aroblin          ###   ########.fr       */
+/*   Updated: 2019/02/02 20:21:57 by aroblin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,31 +17,34 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
+# include "../libft/include/libft.h"
 
-int		main(int ac, char **av)
+int		main(int ac, char **av, char **env)
 {
 	pid_t pid;
 	int status;
-	char	*command = "ls";
+	char	*command = "/bin/cat";
 	int		ret;
+	int		fd;
+	char	**tab;
+	int		fd_pip[2];
 
-	ret = open("out", O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
+	char	mess[100] = "bonjour";
+
+	tab  = ft_strsplit(command, ' ');
+
+	//fd = open("out", O_WRONLY | O_CREAT | O_APPEND, S_IRWXU); // double redr droite
+	//fd = open("out", O_WRONLY | O_CREAT, S_IRWXU);// simple redir == ecrase
+	fd = open("out", O_RDONLY, S_IRWXU);// lecture depuis fichier
 	status = 0;
-//	pid = fork();
-//	if (pid == -1)
-//	{
-//		return (-1);
-//	}
-//	else if (pid == 0)
-//	{
-		dup2(ret, 1);
-		if (execve(command, NULL, NULL) < 0)
-		{
-			printf("test\n");
-			exit(0);
-		}
-//	}
-//	else
-//		wait(&status);
+//	dup2(fd, 1);// redir standar droite
+//	dup2(fd, 2);// redir erreur droite
+//	dup2(fd, 0);// redir entre standard
+	if (execve(command, tab, env) < 0)
+	{
+		printf("test\n");
+		exit(0);
+	}
+	close(fd);
 	return (0);
 }
