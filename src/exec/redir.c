55200729667426a6421cb7dac9b335406909b386
Roblin Aurelien <roblinaurelien@gmail.com>
2019-02-05 05:10:41 +0100

    begin redir

--- src/exec/redir.c
+++ src/exec/redir.c
@@ -6,7 +6,7 @@
 /*   By: aroblin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/05 01:40:48 by aroblin           #+#    #+#             */
-/*   Updated: 2019/02/05 01:51:56 by aroblin          ###   ########.fr       */
+/*   Updated: 2019/02/05 04:03:19 by aroblin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,63 @@
 
 void	out_redir(t_cmd_token *cmd, t_token *name, t_token *redir)
 {
-	int		fd;
-	int		fd_op;
+	int				fd;
+	int				fd_op;
+	char			**tab;
+	t_shell_data	*data;
 
-	fd = open(name->token, O_CREAT | O_WRONLY | O_TRUNC); // ajouter mode
-	redir->token 
-	dup2(redir->
-	
+	data = shell_data_singleton();
+	tab = get_cmd_tab(&cmd);
+	fd_op = ft_atoi(redir->token);
+	if ((fd = open(name->token, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR
+		| S_IWUSR | S_IRGRP | S_IROTH) < 0))
+	{
+		ft_printf("erreur open in out_redir"); // a suprimer
+		quit_shell(EXIT_SUCCESS, 1);
+	}
+	dup2(fd_op, fd);
+	exec_command(tab, data->env_tab);
+	close(fd);
+}
+
+void	app_out_redir(t_cmd_token *cmd, t_token *name, t_token *redir)
+{
+	int				fd;
+	int				fd_op;
+	char			**tab;
+	t_shell_data	*data;
+
+	data = shell_data_singleton();
+	tab = get_cmd_tab(&cmd);
+	fd_op = ft_atoi(redir->token);
+	if ((fd = open(name->token, O_APPEND | O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR
+		| S_IWUSR | S_IRGRP | S_IROTH) < 0))
+	{
+		ft_printf("erreur open in out_redir"); // a suprimer
+		quit_shell(EXIT_SUCCESS, 1);
+	}
+	dup2(fd_op, fd);
+	exec_command(tab, data->env_tab);
+	close(fd);
+}
+
+void	in_redir(t_cmd_token *cmd, t_token *name, t_token *redir)
+{
+	int				fd;
+	int				fd_op;
+	char			**tab;
+	t_shell_data	*data;
+
+	data = shell_data_singleton();
+	tab = get_cmd_tab(&cmd);
+	fd_op = ft_atoi(redir->token);
+	if ((fd = open(name->token, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR
+		| S_IWUSR | S_IRGRP | S_IROTH) < 0))
+	{
+		ft_printf("erreur open in out_redir"); // a suprimer
+		quit_shell(EXIT_SUCCESS, 1);
+	}
+	dup2(fd, fd_op);
+	exec_command(tab, data->env_tab);
+	close(fd);
+}
