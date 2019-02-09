1b608300b6e0c39f4ed04abe5bd073612f3c78e1
ndubouil <ndubouil@student.42.fr>
2019-02-09 03:43:07 +0100

    ADD heredoc

--- src/exec/redir_recursion.c
+++ src/exec/redir_recursion.c
@@ -6,11 +6,12 @@
 /*   By: ndubouil <ndubouil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/07 23:01:07 by ndubouil          #+#    #+#             */
-/*   Updated: 2019/02/08 05:28:22 by ndubouil         ###   ########.fr       */
+/*   Updated: 2019/02/09 03:22:50 by ndubouil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "21sh.h"
+#include <sys/types.h>
 
 void 	redir_recursion(t_btree *tree)
 {
@@ -46,7 +47,7 @@ void 	redir_recursion(t_btree *tree)
 		{
 			if (get_type_token(tree->data) == OUT_RDIR_TYPE || get_type_token(tree->data) == OUT_ARDIR_TYPE )
 				arg = 1;
-			else if (get_type_token(tree->data) == IN_RDIR_TYPE)
+			else if (get_type_token(tree->data) == IN_RDIR_TYPE || get_type_token(tree->data) == HEREDOC_TYPE)
 				arg = 0;
 		}
 		if (get_type_token(tree->data) == OUT_RDIR_TYPE || get_type_token(tree->data) == OUT_ARDIR_TYPE )
@@ -58,6 +59,58 @@ void 	redir_recursion(t_btree *tree)
 		{
 			dup2(fd, arg);
 			close(fd);
+		}
+		else if (get_type_token(tree->data) == HEREDOC_TYPE)
+		{
+			char *str;
+			str = NULL;
+			char	*argument;
+			char	*tmp;
+			char *line;
+			line = NULL;
+			static int id = 52;
+
+			id++;
+			tmp = ft_itoa(id);
+			char *path = ft_strjoin("/tmp/.21sh_heredoc_", tmp);
+			argument = get_token_token(tree->right->data);
+			ft_strdel(&tmp);
+			int fd_h = open(path, O_CREAT | O_TRUNC | O_RDWR, 0644);
+			if (fd_h == -1)
+			{
+				ft_printf("21sh: open failed"); // a suprimer
+				quit_shell(EXIT_FAILURE, 0);
+			}
+			while (!ft_strequ(line, argument))
+			{
+				ft_strdel(&line);
+				line = get_line(PROMPT_MIN);
+				if (str && !ft_strequ(line, "\n"))
+				{
+					tmp = str;
+					str = ft_strjoin(tmp, "\n");
+					ft_strdel(&tmp);
+				}
+				if (!ft_strequ(line, argument))
+				{
+					if (!str)
+						str = ft_strdup(line);
+					else
+					{
+						tmp = str;
+						str = ft_strjoin(tmp, line);
+						ft_strdel(&tmp);
+					}
+				}
+			}
+			// ft_fd_printf(fd_h, "%s", str);
+			write(fd_h, str, ft_strlen(str));
+			close(fd_h);
+			ft_strdel(&str);
+			fd_h = open(path, O_RDONLY);
+			ft_strdel(&path);
+			dup2(fd_h, arg);
+			close(fd_h);
 		}
 		return ;
 	}
@@ -100,7 +153,7 @@ void 	redir_recursion(t_btree *tree)
 		}
 		else if (get_type_token(tree->data) == IN_RDIR_TYPE)
 		{
-			dup2(arg, fd);
+			dup2(fd, arg);
 			close(fd);
 		}
 	}
