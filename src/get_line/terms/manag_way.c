792cdf5df3c0298f7ab35007bd3dc732e89241d2
ndubouil <ndubouil@student.42.fr>
2019-02-16 03:24:29 +0100

    prepare the merge

--- src/get_line/terms/manag_way.c
+++ src/get_line/terms/manag_way.c
@@ -6,11 +6,12 @@
 /*   By: aroblin <aroblin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/02 05:06:01 by aroblin           #+#    #+#             */
-/*   Updated: 2019/02/16 02:42:50 by ndubouil         ###   ########.fr       */
+/*   Updated: 2019/02/16 03:20:37 by ndubouil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_line.h"
+#include "libftprintf.h"
 
 static int		ft_end_line(t_term **t, char *cmd)
 {
